aaf4e8c8260786452e1d28f4251ed790cdbcd2b1
hashd <gitk@danduprolu.com>
2017-03-31 01:06:07 +0530

    Add ability to create and persist shortened URLs

---
+++ config.js
@@ -0,0 +1,7 @@
+const config = {
+  dbName: 'imly',
+  dbPort: 27017,
+  dbUrl: 'localhost'
+};
+
+module.exports = config;
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,57 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express'),
+    router = express.Router(),
+    randomString = require('randomstring'),
+    MongoClient = require('mongodb').MongoClient,
+    Server = require('mongodb').Server,
+    config = require('../config'),
+    dbUrl = `mongodb://${config.dbUrl}:${config.dbPort}/${config.dbName}`;
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+let mongoClient = new MongoClient(new Server(config.dbUrl, config.dbPort));
+let generateHash = () => randomString.generate({ length: 8, charset: 'alphanumeric', capitalization: 'lowercase' });
+
+// Integrate server side rendering with UI repo here
+router.get('/', (req, res) => res.render('index', { title: 'Imly!' }));
+
+// Redirect user to the location if hash is found else redirect to home
+router.get('/:hash', (req, res) => {
+  let urlHash = req.params.hash;
+  mongoClient.connect(dbUrl, (err, db) => {
+    db.collection('urls').findOne({ hash: urlHash }, (err, record) => {
+      if (record !== null) {
+        res.redirect(record.url);
+      } else {
+        res.redirect('/');
+      }
+
+      db.close();
+    });
+  });
 });
 
-router.get('/:shortUrl', function (req, res) {
-  res.redirect('http://www.imaginea.com');
+let createHashedUrl = function (db, url, res) {
+  let hash = generateHash();
+  db.collection('urls').findOne({ hash }, (err, record) => {
+    if (record !== null) {
+      createHashedUrl(db, url, res);
+    } else {
+      db.collection('urls').insertOne({ hash, url }, {}, (err) => {
+        if (!err) {
+          db.collection('urls').findOne({ hash }, (err, { hash, url }) => {
+            res.json({ hash, url });
+            db.close();
+          });
+        }
+      })
+    }
+  });
+};
+
+// Create a new IMLY Url, please!
+router.post('/', (req, res) => {
+  let url = req.body.url;
+  mongoClient.connect(dbUrl, function (err, db) {
+    createHashedUrl(db, url, res);
+  });
 });
 
 module.exports = router;
